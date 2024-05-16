255dcf480b2e56293ed10e292039cd04fd75a07f
Rafael <rafael.prevost.15@gmail.com>
2024-05-16 17:49:00 -0400

    Add idris2 setu[

--- init.lua
+++ init.lua
@@ -9,7 +9,10 @@ vim.call('plug#begin')
 Plug ('polirritmico/monokai-nightasty.nvim')
 
 -- Language plugins
-Plug ('edwinb/idris2-vim')
+--Plug ('edwinb/idris2-vim')
+Plug ('neovim/nvim-lspconfig')
+Plug ('MunifTanjim/nui.nvim')
+Plug ('ShinKage/idris2-nvim')
 Plug ('iamcco/markdown-preview.nvim', { ['do'] = 'cd app && npx --yes yarn install'})
 Plug ('neoclide/coc.nvim', {['do'] = 'yarn install --frozen-lockfile'})
 
@@ -26,7 +29,7 @@ vim.opt.background = "dark" -- default to dark or light style
 vim.cmd.colorscheme("monokai-nightasty")
 
 -- Startup config
---
+
 require("startup").setup({theme = "startify"})
 
 -- Nvim tree configuration
@@ -52,6 +55,32 @@ require("nvim-tree").setup({
     dotfiles = true,
   },
 })
+
+-- idris2 setup
+
+
+local function save_hook(action)
+  vim.cmd('silent write')
+end
+
+require('idris2').setup({code_action_post_hook = save_hook})
+
+
+vim.cmd [[nnoremap <Leader>a <Cmd>lua require('idris2.code_action').add_clause();<CR>]]
+vim.cmd [[nnoremap <Leader>c <Cmd>lua require('idris2.code_action').case_split()<CR>]]
+vim.cmd [[nnoremap <Leader>mc <Cmd>lua require('idris2.code_action').make_case()<CR>]]
+vim.cmd [[nnoremap <Leader>w <Cmd>lua require('idris2.code_action').make_with()<CR>]]
+vim.cmd [[nnoremap <Leader>l <Cmd>lua require('idris2.code_action').make_lemma()<CR>]]
+vim.cmd [[nnoremap <Leader>e <Cmd>lua require('idris2.code_action').expr_search()<CR>]]
+vim.cmd [[nnoremap <Leader>r <Cmd>lua require('idris2.code_action').refine_hole()<CR>]]
+vim.cmd [[nnoremap <Leader>g <Cmd>lua require('idris2.code_action').generate_def()<CR>]]
+vim.cmd [[nnoremap <Leader>h <Cmd>lua require('idris2.code_action').expr_search_hints()<CR>]]
+vim.cmd [[nnoremap <Leader>i <Cmd>lua require('idris2.code_action').intro()<CR>]]
+
+vim.cmd [[nnoremap <Leader>s <Cmd>lua require('idris2.semantic').request()<CR>]]
+
+vim.cmd [[nnoremap <Leader>os <Cmd>lua require('idris2.hover').open_split()<CR>]]
+vim.cmd [[nnoremap <Leader>cs <Cmd>lua require('idris2.hover').close_split()<CR>]]
 
 -- Key mapping
 
